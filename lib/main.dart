298452d73697829fd2dd0a013b2c4c0ff16b1e32
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2021-10-31 23:06:07 +0530

    questions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,57 @@
 // Deepansh Dubey.
 // 26/10/2021.
 
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var index = 0;
+  void answer() {
+    index = index + 1;
+    print(index);
+    print('Answer 1 Chosen');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello! Paddi '),
+      home: Scaffold(
+          appBar: AppBar(
+              title: Text(
+            'My First App',
+          )),
+          body: Column(
+            children: [
+              Text(questions[index]),
+              RaisedButton(child: Text('answer 1'), onPressed: answer),
+              RaisedButton(
+                  child: Text('answer 2'),
+                  onPressed: () => print('Answer 2 chosen')),
+              RaisedButton(
+                  child: Text('answer 3'),
+                  onPressed: () {
+                    print('Answer 3 chosen');
+                  }),
+            ],
+          )),
     );
   }
 }
